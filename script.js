68e4ab8171cac6149f6353a66b59feed2f28d29a
Jake <114455978+jlorenzen024@users.noreply.github.com>
2022-12-27 15:18:00 -0500

    Add files via upload
    added scheduling functionality

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function clickHandle2() {
     window.location.href = "contact.html";
 }
+
+scrollTo = (element) => {
+    window.scroll({
+      behavior: 'smooth',
+      left: 0,
+      top: element.offsetTop
+    });
+    console
+  }
+  
+  document.getElementById("header-link-three").addEventListener('click', () => {
+    scrollTo(document.getElementById("fourth-section"));
+  });
